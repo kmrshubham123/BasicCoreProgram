09fb86351323630b8692e6a5e8d993047a404319
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-27 15:59:50 +0530

    [Kumar Shubham]Added: welcome to Basic Core Programs

---
+++ CorePrograms/CorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#### Welcome to Basic Core Programs ####");
+        }
+    }
+}
