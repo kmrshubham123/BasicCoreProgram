a9213fc696fb3aa3c0562270b17c5b8bf7b9b7e5
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-27 16:55:32 +0530

    [Kumar Shubham]Added: calculated Flip coin Head Tail Percentange

---
+++ CorePrograms/CorePrograms/FlipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class FlipCoin
+    {
+        public int heads = 0;
+        public int tails = 0;
+
+
+        public void HeadsTailsPercentage()
+        {
+            Console.WriteLine("Enter How many times you want to Flip a coin :- ");
+            int FlipNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int coinFlip = 0; coinFlip <= FlipNumber; coinFlip++)
+            {
+                Random random = new Random();
+                double FlipCoin = random.NextDouble();
+                if (FlipCoin < 0.5)
+                    tails++;
+                else
+                    heads++;
+            }
+            double PercentageTails = (tails*100) / FlipNumber;
+            double PercentageHeads = (heads*100) / FlipNumber;
+            Console.WriteLine("Percentage of Heads:-" +PercentageHeads);
+            Console.WriteLine("Percentange of Tails:-" +PercentageTails);
+
+        }
+    }
+}
--- CorePrograms/CorePrograms/Program.cs
+++ CorePrograms/CorePrograms/Program.cs
@@ -7,6 +7,21 @@ namespace CorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("#### Welcome to Basic Core Programs ####");
+
+            Console.WriteLine("1:Flip Coin to check Head or tail");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.HeadsTailsPercentage();
+                    break;
+
+
+            }
+
         }
     }
 }
