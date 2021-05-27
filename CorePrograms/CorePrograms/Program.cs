89c0411a14b129aa66ba41ec297f8764ea1c48ac
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-27 17:10:34 +0530

    [Kumar Shubham]Added: Year is Leap year or Normal Year

--- CorePrograms/CorePrograms/Program.cs
+++ CorePrograms/CorePrograms/Program.cs
@@ -9,6 +9,7 @@ namespace CorePrograms
             Console.WriteLine("#### Welcome to Basic Core Programs ####");
 
             Console.WriteLine("1:Flip Coin to check Head or tail");
+            Console.WriteLine("2:Check a year is Leap Year or Not");
 
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -18,7 +19,11 @@ namespace CorePrograms
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.HeadsTailsPercentage();
                     break;
-
+             
+                case 2:
+                    LeapYear leap = new LeapYear();
+                    leap.LeapYr();
+                    break;
 
             }
 
